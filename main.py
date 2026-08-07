a4c575b3ed0178fb2ff232e7fa64096deceb6506
raeul0304 <subong0304@gmail.com>
2026-08-07 11:31:28 +0900

    event filter API 구성

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from services.event_repository import EventRepository
-from services.event_filter import filter_events_by_keywords
+from services.event_filter import get_filter_options_data, filter_and_map_events
 from services.email_generation import generate_email
-from schemas import EmailGenerationRequest, EmailGenerationResponse
+from schemas import EmailGenerationRequest, EmailGenerationResponse, EventSearchRequest, EventSearchResponse
 from models.factory import load_llm
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 실제 운영 환경- 프론트엔드 도메인으로 변경
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+repository = EventRepository()
+
 
 @app.post("/api/events/filter-options")
 def get_event_filter_options():
+    df = repository.load_events_dataframe()
+    result = get_filter_options_data(df)
 
-    return
+    return result
 
 
 
 @app.post("/api/events/search")
-def get_event_filter_options():
+def get_event_filter_options(request: EventSearchRequest):
+    df = repository.load_events_dataframe()
+    events_list = filter_and_map_events(df, request)
 
-    return
+    return {"events": events_list}
